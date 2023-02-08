6d8394b66eb207617293b4701ecf80d80c5fb217
ChinmayMittal <chinmay3102@gmail.com>
2023-02-09 00:05:11 +0530

    ridge regression, scikit-learn and feature selection

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import argparse
 import os
 import pandas as pd
 from utilities import read_data, plot_loss
-from model import LinearRegression
+from model import LinearRegression, ScikitLearnLR
+from sklearn.feature_selection import SelectKBest, SelectFromModel
+import sklearn.linear_model as lm
 
 parser = argparse.ArgumentParser(
                     prog = 'Linear Regression',
@@ -20,10 +22,11 @@ args = parser.parse_args()
 if __name__ == "__main__":
     print(f"Train Path: {args.train_path}", f"Test Path: {args.test_path}", f"Val Path: {args.val_path}", f"Outut Path: {args.out_path}", sep="\n")
     
-    # if not os.path.isdir(args.out_path):
-    #     os.makedirs(args.out_path)
+    output_dir =  os.path.split(args.out_path)[0]
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
         
-    assert (args.section in [1,2,5])
+    assert (args.section in [1,2,3,4,5])  ### 3 is for scikit learn implementation
     
     print(f"Section: {args.section}")
     
@@ -37,18 +40,65 @@ if __name__ == "__main__":
     print(f"Test Data: {test_features.shape}")
     
     ### TRAIN_SETTUP
-    train_X, train_y = train_features, train_scores
-    val_X, val_y = val_features, val_scores
-    num_train_samples, num_features = train_X.shape
-    loss = "MSE"
-    learning_rate = 0.001
-    stopping_criterion = "maxit"
-    max_iters = 100000
+    if not args.section == 4:
+        train_X, train_y = train_features, train_scores
+        val_X, val_y = val_features, val_scores
+        num_train_samples, num_features = train_X.shape
+        learning_rate = None
+    else:
+        ### FEATURE SELECTION   
+        method = "select-from-model"
+        
+        if method == "k-best":
+            
+            ### K-BEST
+            selector = SelectKBest(k=10)
+            selector.fit(train_features, train_scores)
 
-    model = LinearRegression(loss=loss, num_features=num_features, 
+        else:
+            
+            ### SELECT FROM MODEL
+            selector = SelectFromModel(estimator=lm.LinearRegression(), max_features=10)
+            selector.fit(train_features, train_scores)
+            
+            
+        train_X, train_y = selector.transform(train_features), train_scores    
+        val_X, val_y = selector.transform(val_features), val_scores
+        test_features, test_scores = selector.transform(test_features), test_scores
+        num_train_samples, num_features = train_X.shape
+        args.section = 2 ### default to ridge regression with feature selection 
+        print(f"Reduced Train Data Shape: {train_X.shape}, Val Data: {val_X.shape}")
+        
+        learning_rate = 0.05
+        
+        
+    if learning_rate is None:
+        learning_rate = 0.001
+    # stopping_criterion = "maxit" ### or "reltol"
+    stopping_criterion = "reltol"
+    max_iters = 10000 ### for "maxit"
+    val_loss_decrease_threshold = 0.01 ### for "reltol"
+    model = None
+    if args.section == 1:
+        loss = "MSE"
+        model = LinearRegression(loss=loss, num_features=num_features, 
                                         learning_rate=learning_rate, 
                                         stopping_criterion=stopping_criterion,
-                                        max_iters = max_iters)
+                                        max_iters = max_iters,
+                                        val_loss_decrease_threshold=val_loss_decrease_threshold)
+    elif args.section == 2:
+        loss = "ridge"
+        lambda_ = 5
+        model = LinearRegression(loss=loss, num_features=num_features, 
+                                        learning_rate=learning_rate, 
+                                        stopping_criterion=stopping_criterion,
+                                        max_iters = max_iters,
+                                        val_loss_decrease_threshold=val_loss_decrease_threshold,
+                                        lambda_=lambda_)
+    elif args.section == 3:
+        model = ScikitLearnLR()
+        
+            
     
     ### TRAINING_LOOP
     print("Training Started ... ")
@@ -58,11 +108,13 @@ if __name__ == "__main__":
         if(model.training_finished()):
             break
         
-    
     print("Training Finished ... ")
     
+    print(f"Training MSE: {model.custom_loss(train_X, train_y, loss='MSE')}, Val MSE: {model.custom_loss(val_X, val_y, loss='MSE')}")
+    print(f"Training MAE: {model.custom_loss(train_X, train_y, loss='MAE')}, Val MAE: {model.custom_loss(val_X, val_y, loss='MAE')}")
     ### LOSS CURVES
-    plot_loss(model.train_loss, model.val_loss)
+    if(model.train_loss is not None):
+        plot_loss(model.train_loss, model.val_loss)
     
     print("Inference on Test Set ... ")
     test_pred = model.pred(test_features)
@@ -73,7 +125,6 @@ if __name__ == "__main__":
     }
     test_df = pd.DataFrame(data=test_data)
     test_df.to_csv(args.out_path, header=False, index=False, sep=" ")
-    print(test_df)
     
     
 
--- model.py
+++ model.py
@@ -1,12 +1,22 @@
 import numpy as np
-
+import sklearn.linear_model as lm
 
 
 class LinearRegression():
     
     
-    def __init__(self, loss="MSE", num_features=None, learning_rate=0.001, stopping_criterion="maxit", max_iters=10):
-        
+    def __init__(self, loss="MSE", num_features=None, learning_rate=0.001, stopping_criterion="maxit", max_iters=10, val_loss_decrease_threshold = .01, lambda_ = 5):
+        """_summary_
+
+        Args:
+            loss (str, optional): used for computing gradients. Defaults to "MSE". | options "ridge" | "MSE"
+            num_features (_type_, optional): number of features in the model. Defaults to None.
+            learning_rate (float, optional): learning rate for gradient descent. Defaults to 0.001.
+            stopping_criterion (str, optional): stopping criterion using validation loss. Defaults to "maxit". | options "reltol" and "maxit"
+            max_iters (int, optional): max iterations for stopping_criterion => "maxit". Defaults to 10.
+            val_loss_decrease_threshold (float, optional): val loss decrease threshold for stopping_criterion => "reltol" . Defaults to .01.
+            lambda_ : float : regularization parameter for ridge regression 
+        """
         self.error_metric = loss ### MSE or Ridge Loss
         self.num_features = num_features ### without the bias term
         self.w = np.zeros((self.num_features+1,), dtype=np.float64) ### first term is bias
@@ -16,6 +26,8 @@ class LinearRegression():
         self.train_loss = []
         self.val_loss = []
         self.num_updates = 0
+        self.val_loss_decrease_threshold = val_loss_decrease_threshold
+        self.lambda_ = lambda_
         
     def add_bias_column(self, X):
         ### add the bias column of all ones to the X (as first column)
@@ -23,6 +35,17 @@ class LinearRegression():
         ones = np.ones((len(X), 1), dtype=np.float64) #### N * 1
         one_X = np.concatenate((ones, X), axis=1)
         return one_X
+    
+    def custom_loss(self, X, y, loss = "MSE", bias_added=False):
+        #### X is N*d, y => N
+        if not bias_added:
+            X = self.add_bias_column(X)
+        pred = np.dot(X, self.w)
+        error = pred - y
+        if loss == "MSE":
+            return np.mean(error**2)
+        elif loss == "MAE":
+            return np.mean(np.abs(error))
         
     def loss(self, X, y):
         ### X is N * d+1, y => N 
@@ -30,17 +53,23 @@ class LinearRegression():
         error = pred  - y
         if self.error_metric == "MSE":
             error = np.sum(error**2)/(2*len(X))
+        elif self.error_metric == "ridge":
+            error = np.sum(error**2)/(2*(len(X))) + self.lambda_ * (np.abs(self.w**2) - self.w[0]**2)
     
         return error
     
     def gradient(self, X, y):
         ### X => N * d+1, y => N
         pred = np.dot(X, self.w )
+        error = (pred - y) 
+        error = np.reshape(error, newshape=(-1, 1))
+        grad_matrix = X * error
         if self.error_metric == "MSE":
-            error = (pred - y) 
-            error = np.reshape(error, newshape=(-1, 1))
-            grad_matrix = X * error
             grad = np.mean(grad_matrix, axis=0)
+        elif self.error_metric == "ridge":
+            regularizing_term = 2 * self.lambda_ * self.w
+            regularizing_term[0] = 0 ### don't regularize bias
+            grad = np.mean(grad_matrix, axis=0) + regularizing_term
         
         return grad
             
@@ -49,6 +78,13 @@ class LinearRegression():
         
         if self.stopping_criterion == "maxit":
             return self.num_updates >= self.max_iters
+        elif self.stopping_criterion == "reltol":
+            if(len(self.val_loss) < 2):
+                return False
+            else:
+                val_loss_decrease = self.val_loss[-1]-self.val_loss[-2]
+                relative_decrease = val_loss_decrease / (self.val_loss[-2])
+                return -1*relative_decrease < self.val_loss_decrease_threshold        
     
         
     def update_weights(self, train_X, train_y, val_X, val_y):
@@ -57,16 +93,16 @@ class LinearRegression():
         val_X = self.add_bias_column(val_X)
 
         if(self.num_updates == 0):
-            self.train_loss.append(self.loss(train_X, train_y))
-            self.val_loss.append(self.loss(val_X, val_y))
+            self.train_loss.append(self.custom_loss(train_X, train_y, loss="MSE", bias_added=True))
+            self.val_loss.append(self.custom_loss(val_X, val_y, loss="MSE", bias_added=True))
 
         gradient_t = self.gradient(train_X, train_y)
         self.w = self.w - self.learning_rate * gradient_t
 
         self.num_updates += 1
         
-        self.train_loss.append(self.loss(train_X, train_y))
-        self.val_loss.append(self.loss(val_X, val_y))
+        self.train_loss.append(self.custom_loss(train_X, train_y, loss="MSE", bias_added=True))
+        self.val_loss.append(self.custom_loss(val_X, val_y, loss="MSE", bias_added=True))
         
     def pred(self, X):
 
@@ -76,5 +112,37 @@ class LinearRegression():
         return pred
         
         
-        
+class ScikitLearnLR():
     
+    
+    def __init__(self):
+        self.model = lm.LinearRegression(fit_intercept=True, n_jobs=-1)
+        self.train_loss = None
+        self.val_loss = None
+        
+    def add_bias_column(self, X):
+        ### add the bias column of all ones to the X (as first column)
+        ### transform it form N*d => N*d+1 
+        ones = np.ones((len(X), 1), dtype=np.float64) #### N * 1
+        one_X = np.concatenate((ones, X), axis=1)
+        return one_X
+    
+    def custom_loss(self, X, y, loss = "MSE", bias_added=False):
+        #### X is N*d, y => N
+        pred = self.model.predict(X)
+        error = pred - y
+        if loss == "MSE":
+            return np.mean(error**2)
+        elif loss == "MAE":
+            return np.mean(np.abs(error))
+        
+    def update_weights(self, train_X, train_y, val_X, val_y):
+        self.model.fit(train_X, train_y)
+    
+    
+    def training_finished(self):
+        return True ### only one pass for scikit learn
+    
+    def pred(self, X):
+        ## X is n*d
+        return self.model.predict(X)
