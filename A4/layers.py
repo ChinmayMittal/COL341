cf6db134b70b7d460ffbd117b773e10d4b216857
ChinmayMittal <chinmay3102@gmail.com>
2023-04-26 16:34:52 +0530

    removed channel for loops from backward pass

--- A4/layers.py
+++ A4/layers.py
@@ -95,19 +95,18 @@ class MaxPool2D():
         (N, C_out, H_out, W_out) = delY.shape
         output = np.zeros(self.cache.shape) ## shape of input to this layer
         ## iterating over the output
-        for channel in range(C_out):
-            for h in range(H_out):
-                for w in range(W_out):
-                    ### input to consider
-                    h_start, h_end = h * self.stride, h * self.stride + self.kernel_size
-                    w_start, w_end = w * self.stride, w * self.stride + self.kernel_size
-                    input_slice = self.cache[:, channel, h_start:h_end, w_start:w_end] ## input which computed this output ||| B * K * K 
-                    max_value = np.max(input_slice.reshape(N, -1), axis=1) ### B, max_value per batch in this window
-                    ## [TODO] What if multiple max values exist ?
-                    max_mask = ( input_slice == max_value.reshape((-1, 1, 1)))  ### B * K * K ### mask indicating presence of max value
-                    output[:, channel, h_start:h_end, w_start:w_end] = max_mask.astype(np.int32) * delY[:, channel, h, w].reshape((-1, 1, 1))  ### B * K * K
+        # for channel in range(C_out):
+        for h in range(H_out):
+            for w in range(W_out):
+                ### input to consider
+                h_start, h_end = h * self.stride, h * self.stride + self.kernel_size
+                w_start, w_end = w * self.stride, w * self.stride + self.kernel_size
+                input_slice = self.cache[:, :, h_start:h_end, w_start:w_end] ## input which computed this output ||| B * C * K * K 
+                max_value = np.max(input_slice.reshape(N, C_out, -1), axis=2) ### (B, C) max_value per batch in this window
+                ## [TODO] What if multiple max values exist ?
+                max_mask = ( input_slice == max_value.reshape((-1, C_out, 1, 1)))  ### B * C *  K * K ### mask indicating presence of max value
+                output[:, :, h_start:h_end, w_start:w_end] = max_mask.astype(np.int32) * delY[:, :, h, w].reshape((-1, C_out, 1, 1))  ### B * C *  K * K
         return output
-    
     
 class Conv2D():
     
@@ -148,18 +147,17 @@ class Conv2D():
         self.b_grad = np.sum(np.moveaxis(delY, source=1, destination=0).reshape(C_out, -1), axis=1) ### for each channel sum all the gradient values
         
         self.W_grad = np.zeros(shape=self.W.shape) ### C_out * C_in * K * K
-        for c in range(C_out):
-            for h in range(H_out):
-                for w in range(W_out):
-                    ### input to consider
-                    h_start, h_end = h * self.stride, h * self.stride + self.kernel_size
-                    w_start, w_end = w * self.stride, w * self.stride + self.kernel_size
-                    input_slice = self.cache[:, :, h_start:h_end, w_start:w_end] ## B * C_in * K * K
-                    ## C_in * K * K            B * C_in * K * K  ||| B, 
-                    self.W_grad[c, :, :, :] += np.sum(input_slice * (delY[:, c, h, w].reshape(N, 1, 1, 1)), axis=0)
-                    ## B * C_in * K * K                               1 * C_in * K * K  ||| B, 1, 1, 1
-                    output[:, :, h_start:h_end, w_start:w_end] += np.expand_dims(self.W[c, :, :, :], axis=0) * delY[:, c, h, w].reshape(N, 1, 1, 1)
-        
+        for h in range(H_out):
+            for w in range(W_out):
+                ### input to consider
+                h_start, h_end = h * self.stride, h * self.stride + self.kernel_size
+                w_start, w_end = w * self.stride, w * self.stride + self.kernel_size
+                input_slice = self.cache[:, :, h_start:h_end, w_start:w_end] ## B * C_in * K * K
+                ## C_out * C_in * K * K            B * _ * C_in * K * K  ||| B, C_out , _1_, _1_, _1_
+                self.W_grad[:, :, :, :] += np.sum(np.expand_dims(input_slice, axis=1) * (delY[:, :, h, w].reshape(N, C_out, 1, 1, 1)), axis=0)
+                ## B * C_in * K * K                               C_out * C_in * K * K  ||| B, C_out, 1, 1  ==>  _1_ * C_out * C_in * K * K  || B * C_out * _1_ * 1 * 1 => then sum across outputs channels
+                output[:, :, h_start:h_end, w_start:w_end] += np.sum(np.expand_dims(self.W[:, :, :, :], axis=0) * delY[:, :, h, w].reshape(N, C_out, 1, 1, 1), axis=1)
+    
         return output
     
     def update(self, lr):
--- A4/scratch.py
+++ A4/scratch.py
@@ -5,14 +5,14 @@ from cnn import CNN
 from tqdm import tqdm
 
 ### PARAMS
-BATCH_SIZE = 16
+BATCH_SIZE = 32
 NUM_CLASSES = 10
 LEARNING_RATE = 1e-2
 NUM_EPOCHS = 100
 PRINT_INTERVAL = 2000
 SMOOTHING_FACTOR = 0.9
 
-dataset = Dataset("./data/cifar-10-batches-py", sample_interval=100)
+dataset = Dataset("./data/cifar-10-batches-py", batch_size=BATCH_SIZE, sample_interval=1)
 model = CNN(NUM_CLASSES=NUM_CLASSES)
 
 for epoch in range(NUM_EPOCHS):
